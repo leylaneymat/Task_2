2f7991ea42c2d39cb5408aaa10bdbd97c209a357
leylaneymat <lneymat17256@ada.edu.az>
2024-07-21 22:00:18 +0400

    Added task 12

--- Algo-12/script.js
+++ Algo-12/script.js
@@ -1,16 +1,7 @@
-let array = [1, 2, 3, 5, 6]
-
-function eleven(array) {
-    let min = array[0];
-    let max = array[array.length - 1];
-    let missingNumber = 0;
-    array.sort((a, b) => a - b);
-    for (let i = min; i <= max; i++) 
-        if (!array.includes(i)) {
-            missingNumber = i;
-            break;
-        }
-    console.log(missingNumber)
+function twelve(number) {
+    for (let i = 1; i <= 9; i++)
+        console.log(`${i} * ${number} = ${i * number}`)
 }
 
-eleven(array)
+twelve(6)
+twelve(3)
